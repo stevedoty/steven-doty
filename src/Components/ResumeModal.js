1d3e749d10c355e017b233b99366d5f7a9bb4640
Steve Doty <stevedoty16@gmail.com>
2020-09-21 11:06:57 -0700

    create basic ResumeModal

--- src/Components/ResumeModal.js
+++ src/Components/ResumeModal.js
@@ -6,54 +6,53 @@ class ResumeModal extends Component{
 
   render(){
 
-    // function MyVerticallyCenteredModal(props) {
-    //   return (
-    //     <Modal
-    //       {...props}
-    //       size="lg"
-    //       aria-labelledby="contained-modal-title-vcenter"
-    //       centered
-    //     >
-    //       <Modal.Header closeButton>
-    //         <Modal.Title id="contained-modal-title-vcenter">
-    //           Modal heading
-    //         </Modal.Title>
-    //       </Modal.Header>
-    //       <Modal.Body>
-    //         <h4>Centered Modal</h4>
-    //         <p>
-    //           Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-    //           dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-    //           consectetur ac, vestibulum at eros.
-    //         </p>
-    //       </Modal.Body>
-    //       <Modal.Footer>
-    //         <Button onClick={props.onHide}>Close</Button>
-    //       </Modal.Footer>
-    //     </Modal>
-    //   );
-    // }
-    //
-    // function App() {
-    //   const [modalShow, setModalShow] = React.useState(false);
-    //
-    //   return (
-    //     <>
-    //       <Button variant="primary" onClick={() => setModalShow(true)}>
-    //         Launch vertically centered modal
-    //       </Button>
-    //
-    //       <MyVerticallyCenteredModal
-    //         show={modalShow}
-    //         onHide={() => setModalShow(false)}
-    //       />
-    //     </>
-    //   );
-    // }
+    function MyVerticallyCenteredModal(props) {
+      return (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Steven Doty
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Resume</h4>
+            <p>
+              My resume will go here
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
+
+    function App() {
+      const [modalShow, setModalShow] = React.useState(false);
+
+      return (
+        <div style={{textAlign:"center"}}>
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            View Resume
+          </Button>
+
+          <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        </div >
+      );
+    }
 
 
-
-    return(<div>hello</div>)
+    return(
+        <App/>
+    )
   }
 }
 
