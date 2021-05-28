9b9ef3e559758acdc8ecca1aba5a5e3f6fd62552
Steve Doty <stevedoty16@gmail.com>
2021-05-28 10:37:01 -0700

    Updates

---
+++ precache-manifest.3e6c3557e042a44f97c1019070cbb24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12857df232a3f8b62d7ee94cd9c7af9e",
+    "url": "/steven-doty/index.html"
+  },
+  {
+    "revision": "65844660e6968b61213b",
+    "url": "/steven-doty/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "eb1ac3e1b769f3055add",
+    "url": "/steven-doty/static/css/main.71aa5be1.chunk.css"
+  },
+  {
+    "revision": "65844660e6968b61213b",
+    "url": "/steven-doty/static/js/2.731bb099.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/steven-doty/static/js/2.731bb099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1ac3e1b769f3055add",
+    "url": "/steven-doty/static/js/main.44e9e8eb.chunk.js"
+  },
+  {
+    "revision": "c762a14de53e6529d96e",
+    "url": "/steven-doty/static/js/runtime-main.c1e39bc4.js"
+  }
+]);
